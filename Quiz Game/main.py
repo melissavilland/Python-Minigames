ac832c6e81253705859763befb7c02bfdff42ffa
Melissa Villand <94393063+melissavilland@users.noreply.github.com>
2022-03-25 23:29:34 +0200

    Add files via upload

---
+++ Quiz Game/main.py
@@ -0,0 +1,13 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for q in question_data:
+    new_question = Question(q['question'], q['correct_answer'])
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
